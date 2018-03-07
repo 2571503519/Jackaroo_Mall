b664d37afcafce54a95afde487688f5e73561e71
Jackaroo_zZ <2571503519@qq.com>
2018-03-07 16:38:01 +0800

    project init finished!

---
+++ src/main/java/com/jackaroo/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.jackaroo.dao;
+
+import com.jackaroo.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/jackaroo/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.jackaroo.dao;
+
+import com.jackaroo.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
